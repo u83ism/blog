0b28620a479ea8a7547aae47d2c2936f4921de7e
ゆうやみ(yu-yami) <u83ism@gmail.com>
2026-05-16 13:52:16 +0900

    💄➕ Github Alert形式に対応

--- astro.config.ts
+++ astro.config.ts
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import remarkGithubAlerts from 'remark-github-alerts';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [sitemap()],
+  markdown: {
+    remarkPlugins: [remarkGithubAlerts],
+  },
   // ホバー時にページを事前取得してページ遷移を高速化
   prefetch: true,
   // GitHub Pagesのリポジトリ名をbaseとして設定
